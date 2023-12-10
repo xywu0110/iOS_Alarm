6956bf9c4727bd43853e921a87fdb04da9f1d83e
xywu0110 <wxiaoyang@hotmail.com>
2023-12-10 23:35:45 +0800

    init project

---
+++ alarm_OC/MainPageViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainPageViewController.h
+//  alarm_OC
+//
+//  Created by XiaoyangWu on 2023/12/10.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainPageViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ alarm_OC/MainPageViewController.m
@@ -0,0 +1,25 @@
+//
+//  MainPageViewController.m
+//  alarm_OC
+//
+//  Created by XiaoyangWu on 2023/12/10.
+//
+
+#import "MainPageViewController.h"
+
+@interface MainPageViewController ()
+
+@end
+
+@implementation MainPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupUI];
+}
+
+- (void)setupUI {
+    self.view.backgroundColor = [UIColor grayColor];
+}
+
+@end
